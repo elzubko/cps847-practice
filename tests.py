07aeb255f415b3989f4076173c61b3909179c253
Tony Misic <tony.misic98@gmail.com>
2020-03-01 16:58:45 -0500

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,7 @@
+import unittest
+import awesome
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(awesome.smile(), ":)")
+if __name__ == '__main__':
+    unittest.main()
